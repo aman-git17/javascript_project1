0032563463c93536aaee1d63729a3324fa550236
aman-git17 <amanrai52618@gmail.com>
2025-03-23 15:38:48 +0530

    Add files via upload

---
+++ java!.js
@@ -0,0 +1,10 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours().toString().padStart(2, '0');
+    var minutes = now.getMinutes().toString().padStart(2, '0');
+    var seconds = now.getSeconds().toString().padStart(2, '0');
+    document.getElementById("clock").innerText = hours + ":" + minutes + ":" + seconds;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
